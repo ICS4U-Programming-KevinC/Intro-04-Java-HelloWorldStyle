545d7d9265d3fba728710bfe658e6ebe7a6e7432
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-09 03:28:04 +0000

    Created HelloWorldStyle.java

---
+++ HelloWorldStyle.java
@@ -0,0 +1,26 @@
+/**
+* Its Hello World, but with style.
+*
+* @author  Kevin Csiffary
+* @version 1.0
+* @since   2024-02-08
+*/
+
+public final class HelloWorldStyle {
+    /**
+    * Private constructor to prevent instantiation of this utility class.
+    */
+    private HelloWorldStyle() {
+        // Prevents instantiation.
+    }
+
+    /**
+    * Create main function.
+    *
+    * @param args Command line arguments.
+    */
+    public static void main(String[] args) {
+        // Display Hello World! to the console
+        System.out.println("Hello World!");
+    }
+}
